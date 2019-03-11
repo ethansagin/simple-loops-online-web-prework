4213dc0c9d915c946dee3e2f8aa17def5ca0bb9f
Ethan <ethansagin@gmail.com>
2019-03-11 22:08:34 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -6,8 +6,7 @@ def loop_iterator(number_of_times)
   loop
     puts phrase
     counter += 1
-    if counter == number_of_times
-      break
+    break if counter == number_of_times
     end
   end
 
